ba726d68657fe62781f739228e233c7f8e7da4fe
Valentine Sénégas <valentinesenegas@hotmail.com>
2022-12-09 17:38:07 -0500

    Deleted problematic final project folder

--- final-project/DBSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const lyrics_collection_Schema = new mongoose.Schema({
-    artist: String,
-    lyrics: String
-})
-
-//3rd parameter: name of collection on MongoDB
-const lyricsCollectionModel = mongoose.model("LyricsModel", lyrics_collection_Schema, "lyrics_collection");
-module.exports = lyricsCollectionModel;
